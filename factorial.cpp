d8930e3866a87aef738ea27ad0afdeea7e9df94f
NISHA GUPTA <59205290+Nishagupta14@users.noreply.github.com>
2021-07-01 23:52:12 +0530

    Add files via upload
    Simple factorial program

---
+++ factorial.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    long long fact=1;
+    for(int i=2;i<=n;i++){
+        fact=(fact*i);
+    }
+    cout<<fact<<endl;
+}
+}
