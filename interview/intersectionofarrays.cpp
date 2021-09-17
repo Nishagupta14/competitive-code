a53d9a3ac85d462b57a069e923db479348e77148
NISHA GUPTA <59205290+Nishagupta14@users.noreply.github.com>
2021-09-17 17:24:12 +0530

    Add files via upload

---
+++ interview/intersectionofarrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int>m;
+        vector<int>res;
+        for(int i=0;i<nums1.size();i++){
+            m[nums1[i]]++;
+        }
+        for(int j=0;j<nums2.size();j++){
+            if(--m[nums2[j]]>=0){
+                res.push_back(nums2[j]);
+            }
+        }
+        return res;
+    }
+};
