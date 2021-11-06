e621904ab8ca19d1400f07295adf585d2b3e965c
NISHA GUPTA <59205290+Nishagupta14@users.noreply.github.com>
2021-11-06 12:24:43 +0530

    Add files via upload

---
+++ interview/findmissingno.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<utility>
+#include<set>
+#include<stack>
+#include<unordered_map>
+#include<unordered_set>
+typedef long long ll;
+#define mod 1000000007
+#define pb push_back
+#define loop(i,s,e) for(int i=s;i<e;i++)
+using namespace std;
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++){
+       cin>>a[i];
+   }
+   int total=(n+2)*(n+1)/2;
+   for(int i=0;i<n;i++){
+       total-=a[i];
+   }
+   cout<<total;
+   //1.XOR
+public int missingNumber(int[] nums) { //xor
+    int res = nums.length;
+    for(int i=0; i<nums.length; i++){
+        res ^= i;
+        res ^= nums[i];
+    }
+    return res;
+}
+//2.SUM
+public int missingNumber(int[] nums) { //sum
+    int len = nums.length;
+    int sum = (0+len)*(len+1)/2;
+    for(int i=0; i<len; i++)
+        sum-=nums[i];
+    return sum;
+}
+//3.Binary Search
+public int missingNumber(int[] nums) { //binary search
+    Arrays.sort(nums);
+    int left = 0, right = nums.length, mid= (left + right)/2;
+    while(left<right){
+        mid = (left + right)/2;
+        if(nums[mid]>mid) 
+         right = mid;
+        else 
+         left = mid+1;
+    }
+    return left;
+    return 0;
+}
